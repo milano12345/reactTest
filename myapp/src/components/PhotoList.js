77d4d8b01286427f05a5bdc2a054714c066873b4
Daniel <dmilano857@gmail.com>
2022-03-18 14:05:30 -0600

    added components

--- myapp/src/components/PhotoList.js
+++ myapp/src/components/PhotoList.js
@@ -4,12 +4,11 @@ import { usePhotos } from './PhotoProvider';
 export function PhotoList() {
     const photos = usePhotos()
     console.log('newphoto',photos)
-  
-    
+
     return (
         <div>
-    {photos.map(function(item, i){
-        console.log('item',item);
+        {photos.map(function(item, i){
+            console.log('item',item);
          return <li key={i}>Test</li>
 })}
         </div>
